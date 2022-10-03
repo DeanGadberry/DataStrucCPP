0b32e4a20658623113e07199364edc0775db2284
Colten <mikulastikc@gmail.com>
2022-10-02 20:51:14 -0500

    the creation of the repo

---
+++ LinkedList/LinkedList/LinkedList.cpp
@@ -0,0 +1,14 @@
+// LinkedList.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    int x = 10;
+    int *ptr = &x;
+
+    std::cout << x << std::endl;
+    std::cout << *ptr << std::endl;
+
+}
