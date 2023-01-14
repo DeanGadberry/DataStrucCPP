8ec00aa79734f451471980832db0c5e46624d56d
Colten <mikulastikc@gmail.com>
2023-01-14 17:23:49 -0600

    messing with inheritance

---
+++ other_stuff/testinginheritance.cpp
@@ -0,0 +1,25 @@
+
+
+#include <iostream>
+#include "Node.h"
+
+
+class NodeGraph : public Node
+{
+private:
+    Node * 
+
+
+};
+
+
+
+int main()
+{
+
+    int stop{};
+    std::cout << "hello" << std::endl;
+    std::cin >> stop;
+
+    return 0;
+}
